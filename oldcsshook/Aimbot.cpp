a0ec89f6f69c1fed91356a674020371f1d31ab66
nightmare_ <ezbruut@mail.ru>
2024-03-21 22:44:20 +0300

    baim fix

--- oldcsshook/Aimbot.cpp
+++ oldcsshook/Aimbot.cpp
@@ -629,6 +629,11 @@ void Aimbot::Main(CUserCmd* pCmd, BasePlayer* LocalPlayer)
 			else iSpot = g_CVars.Aimbot.Hitbox;
 		}
 		else iSpot = g_CVars.Aimbot.Hitbox;
+		if ( g_CVars.Aimbot.BodyAim && GetAsyncKeyState( g_CVars.Aimbot.BodyAimValue ) )
+		{
+			g_CVars.Aimbot.Hitbox = 9;
+		}
+		else g_CVars.Aimbot.Hitbox = HITBOX_HEAD;
 		/*
 		if (true)
 		{
